05ee3afa9a0fb53961ab25034fba73302aec15d3
mithuchandru <mithuchandru30@gmail.com>
2026-05-29 10:49:43 +0530

    Update HotelController.java

--- src/HotelController.java
+++ src/HotelController.java
@@ -33,15 +33,28 @@ public class HotelController{
         }
     }
 
-    public void checkOut(int roomNo) {
-        try {
-            String query="UPDATE rooms SET customer_name=NULL, phone_no=NULL, status='Available' WHERE room_no=?";
-            PreparedStatement pst=con.prepareStatement(query);
-            pst.setInt(1, roomNo);
-            pst.executeUpdate();
-            System.out.println("Check Out Successful");
-        } catch (Exception e) {
-            System.out.println(e);
+   public void checkOut(int roomNo) {
+    try {
+        String checkQuery = "SELECT status FROM rooms WHERE room_no=?";
+        PreparedStatement checkPst=con.prepareStatement(checkQuery);
+        checkPst.setInt(1, roomNo);
+        ResultSet rs = checkPst.executeQuery();
+        if (rs.next()) {
+            String status = rs.getString("status");
+            if (status.equals("Available")) {
+                System.out.println( "Room Already Not Booked");
+            } else {
+                String query ="UPDATE rooms SET customer_name=NULL, phone_no=NULL, status='Available' WHERE room_no=?";
+                PreparedStatement pst =con.prepareStatement(query);
+                pst.setInt(1, roomNo);
+                pst.executeUpdate();
+                System.out.println( "Check Out Successful");
+            }
+        } else {
+            System.out.println("Room Not Found");
         }
+    } catch (Exception e) {
+        System.out.println(e);
     }
+}
 }
